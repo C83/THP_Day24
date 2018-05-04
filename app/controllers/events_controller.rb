12d334a042fe50f666d57f38efd7947ae9082720
Cyril <manda.cyril@gmail.com>
2018-05-04 04:40:29 +0200

    Projet jusqu'à 3.2 + 3.5

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
 	def new
 		@this_event = Event.new
 		# Permet de palier rapidement le décallage horaire et gestion du +2GMT 
-		@this_event.date = Time.now + 2.days + 2.hours
+		@this_event.date = DateTime.now + 2.days + 2.hours
 	end
 
 	def create
@@ -20,6 +20,16 @@ class EventsController < ApplicationController
 
 	def index
 		@list_events = Event.all
+		@list_past_events = Array.new
+		@list_future_events = Array.new
+		@list_events.each do |event|
+			if (event.date - Time.now > 0)
+				# la date de l'évènement est future
+				@list_future_events.push(event)
+			else
+				@list_past_events.push(event)
+			end
+		end
 	end
 
 	def show
